579aa8210dfb9e4ece76d5b3b10d880ab340d679
Sandeep Rathore <Sam@Webonises-MacBook-Pro.local>
2016-02-11 15:14:16 +0530

    Fixed Status bar and button overlay issue

--- app/src/main/java/com/webonise/gardenIt/activities/PlantDetailsActivity.java
+++ app/src/main/java/com/webonise/gardenIt/activities/PlantDetailsActivity.java
@@ -257,7 +257,8 @@ public class PlantDetailsActivity extends AppCompatActivity implements View.OnCl
             textView.setText(getString(R.string.logs));
             textView.setGravity(Gravity.CENTER_HORIZONTAL);
             textView.setTextColor(getResources().getColor(R.color.text_color_green));
-            textView.setTextSize(new DisplayUtil(PlantDetailsActivity.this).dpToPx(6));
+            //textView.setTextSize(new DisplayUtil(PlantDetailsActivity.this).dpToPx(6));
+            textView.setTextAppearance(this, android.R.style.TextAppearance_Large);
             llLogHolder.addView(textView);
             View view = null;
             for (int i = 0; i < sortedList.size(); i++) {
